41049ee1d1a1b397b725a335b2612866f9eb5ee2
Leighton Sapir <leigh@incsub.com>
2018-12-21 17:43:30 -0500

    [update] index call main component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Home from './page-home';
+import Main from './main';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <Home />, document.getElementById( 'showcase' ) );
+ReactDOM.render( <Main />, document.getElementById( 'showcase' ) );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

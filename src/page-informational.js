2f825e07cec87b71753c47a355632ce841f939e1
Leighton Sapir <leigh@incsub.com>
2018-12-21 17:44:09 -0500

    [new] page: informational

---
+++ src/page-informational.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class PageInfo extends Component {
+	render() {
+		return (
+			<React.Fragment>
+
+				<section>
+
+					<h1>Embedded Informational</h1>
+
+					<h2>Sample #1</h2>
+
+					<ul>
+						<li><strong>Image Display:</strong> Left</li>
+						<li><strong>Image Fitting:</strong> Contain</li>
+					</ul>
+
+				</section>
+
+			</React.Fragment>
+		);
+	}
+}

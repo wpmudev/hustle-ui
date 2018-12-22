8cd1dbe57fb1bf4a5cd2ce28c2416617b3b1568b
Leighton Sapir <leigh@incsub.com>
2018-12-22 07:02:19 -0500

    [update] content focus layout samples

--- src/settings/containers/opt-in/layout/focus-content.js
+++ src/settings/containers/opt-in/layout/focus-content.js
@@ -1,9 +1,97 @@
 import React, { Component } from 'react';
 
+import LayoutForm from '../layout-element/form';
+import LayoutContent from '../layout-element/content';
+import LayoutImage from '../layout-element/image';
+import NeverSeeLink from '../never-see-link';
+
 export default class LayoutFocusContent extends Component {
 	render() {
+		let renderContent = '';
+		let renderSidebarLeft = '';
+		let renderSidebarRight = '';
+
+		const imagePosition = this.props.imagePosition ? this.props.imagePosition : 'left';
+		const imagePosClass = 'hustle-layout-position--' + imagePosition;
+
+		const formFields = React.Children.map(
+			this.props.children,
+			child => {
+				return child;
+			}
+		);
+
+		const renderSidebar = (
+			<div className="hustle-layout-sidebar">
+
+				<LayoutImage
+					image={ this.props.image ? this.props.image : '' }
+					imageFit={ this.props.imageFit ? this.props.imageFit : 'none' }
+					imagePosX={ this.props.imagePosX ? this.props.imagePosX : 'center' }
+					imagePosY={ this.props.imagePosY ? this.props.imagePosY : 'center' }
+				/>
+
+				<LayoutForm
+					moduleId={ this.props.moduleId ? this.props.moduleId : '' }
+					icon={ this.props.fieldsIcon ? this.props.fieldsIcon : '' }
+					proximity={ this.props.fieldsProximity ? this.props.fieldsProximity : '' }
+					fieldsGdpr={ this.props.fieldsGdpr ? this.props.fieldsGdpr : '' }
+				>
+
+					{ formFields }
+
+				</LayoutForm>
+
+			</div>
+		);
+
+		if (
+			'' !== this.props.title ||
+			'' !== this.props.subtitle ||
+			'' !== this.props.content ||
+			'' !== this.props.ctaButton
+		) {
+			renderContent = (
+				<div className="hustle-layout-content">
+
+					<LayoutContent
+						title={ this.props.title ? this.props.title : '' }
+						subtitle={ this.props.subtitle ? this.props.subtitle : '' }
+						content={ this.props.content ? this.props.content : '' }
+						ctaButton={ this.props.ctaButton ? this.props.ctaButton : '' }
+					/>
+
+				</div>
+			);
+		}
+
+		if ( 'left' === imagePosition || 'above' === imagePosition ) {
+			renderSidebarLeft = renderSidebar;
+		}
+
+		if ( 'right' === imagePosition || 'below' === imagePosition ) {
+			renderSidebarRight = renderSidebar;
+		}
+
 		return (
-			<p>Content focus layout for opt-ins.</p>
+			<div className="hustle-layout">
+
+				<div className={ `hustle-layout-body ${ imagePosClass }` }>
+
+					{ renderSidebarLeft }
+
+					{ renderContent }
+
+					{ renderSidebarRight }
+
+				</div>
+
+				<NeverSeeLink
+					type={ this.props.type ? this.props.type : '' }
+					NeverSeeLink={ this.props.neverSeeLink ? this.props.neverSeeLink : '' }
+				/>
+
+			</div>
 		);
 	}
 }

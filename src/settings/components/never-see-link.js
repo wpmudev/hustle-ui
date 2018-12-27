05f5c645e5bf22dad90bd4498e58c04797374125
Leighton Sapir <leigh@incsub.com>
2018-12-27 14:07:20 -0500

    [update] never see again link component

---
+++ src/settings/components/never-see-link.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class NeverSeeLink extends Component {
+	render() {
+		let renderElement = '';
+
+		if ( 'true' === this.props.neverSeeLink ) {
+
+			if ( 'inline' !== this.props.type ) {
+
+				renderElement = (
+					<div className="hustle-layout-footer">
+
+						<p><a href="/">Never see this message again</a></p>
+
+					</div>
+				);
+			}
+		}
+
+		return renderElement;
+	}
+}

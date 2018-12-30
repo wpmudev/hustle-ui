a8b92d006b822cf7257c75f3d0808a7163a83b88
Leighton Sapir <leigh@incsub.com>
2018-12-29 23:49:37 -0500

    [update] [component] button: allow extra class

--- src/settings/components/button.js
+++ src/settings/components/button.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 export default class Button extends Component {
 	render() {
 		const label = this.props.label ? this.props.label : 'Button';
+		let extraClass = '';
+
+		if ( this.props.extraClass && '' !== this.props.extraClass ) {
+			extraClass = ' ' + this.props.extraClass;
+		}
 
 		return (
-			<button className="hustle-button">
+			<button className={ `hustle-button${ extraClass }` }>
 				{ label }
 			</button>
 		);

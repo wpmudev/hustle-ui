644dce41a116dfc330e755ad8b8b608730b0b40a
Leighton Sapir <leigh@incsub.com>
2018-12-29 23:53:39 -0500

    [update] sample optin for home page

--- src/page-home.js
+++ src/page-home.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
 
-import SampleOptinEmbed from './containers/samples/optin-embed';
+import Module from './settings/containers/module';
+import Optin from './settings/containers/opt-in';
 
 export default class PageHome extends Component {
 	render() {
+		const basic = {
+			id: '9',
+			type: 'embedded',
+			layout: 'default',
+			palette: 'gray-slate'
+		};
+
+		const content = {
+			title: 'Malesuada Parturient Tellus',
+			subtitle: (
+				'Duis mollis, est non commodo luctus, nisi erat ' +
+				'porttitor ligula, eget lacinia odio sem nec elit.'
+			),
+			content: (
+				<React.Fragment>
+					<h1>Heading 1 - <a href="/">Link</a></h1>
+					<h2>Heading 2 - <a href="/">Link</a></h2>
+					<h3>Heading 3 - <a href="/">Link</a></h3>
+					<h4>Heading 4 - <a href="/">Link</a></h4>
+					<h5>Heading 5 - <a href="/">Link</a></h5>
+					<h6>Heading 6 - <a href="/">Link</a></h6>
+					<p>Cras mattis consectetur purus sit amet fermentum. <strong>Curabitur blandit tempus porttitor.</strong> Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum <a href="/" target="_blank">massa justo</a> sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+					<ul>
+						<li>List one</li>
+						<li>List two</li>
+						<li>List three</li>
+					</ul>
+					<ol>
+						<li>Item one</li>
+						<li>Item two</li>
+						<li>Item three</li>
+					</ol>
+					<blockquote>
+						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+					</blockquote>
+				</React.Fragment>
+			),
+			button: 'Call To Action'
+		};
+
+		const image = {
+			source: 'horizontal.png',
+			fitting: 'contain',
+			position: 'left'
+		};
+
+		const form = {
+			inputIcon: 'static',
+			mailchimp: 'checkbox',
+			gdpr: (
+				'From time to time we would like to send you offers and ' +
+				'information we think you would be interested in. If you ' +
+				'do not want to receive these, please untick the box.'
+			)
+		};
+
 		return (
 			<div className="showcase-content">
 
@@ -15,7 +72,30 @@ export default class PageHome extends Component {
 
 				<p>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
 
-				<SampleOptinEmbed />
+				<Module
+					moduleId={ basic.id }
+					type={ basic.type }
+					palette={ basic.palette }
+				>
+
+					<Optin
+						moduleId={ basic.id }
+						type={ basic.type }
+						layout={ basic.layout }
+						palette={ basic.palette }
+						title={ content.title }
+						subtitle={ content.subtitle }
+						content={ content.content }
+						ctaButton={ content.button }
+						image={ image.source }
+						imageFit={ image.fitting }
+						imagePosition={ image.position }
+						fieldsIcon={ form.inputIcon }
+						formOptions={ form.mailchimp }
+						fieldsGdpr={ form.gdpr }
+					/>
+
+				</Module>
 
 				<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
 

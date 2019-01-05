ed9acad3487b9dffea8ed195bc02f2045ad37962
Leighton Sapir <leigh@incsub.com>
2019-01-04 23:26:05 -0500

    [update] showcase: close button class

--- src/settings/components/button-close.js
+++ src/settings/components/button-close.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class ButtonClose extends Component {
 	render() {
 		return (
-			<button className="hustle-button-icon">
+			<button className="hustle-button-icon hustle-button-close">
 				<i className="hustle-icon-close" aria-hidden="true" />
 				<span className="hustle-screen-reader">Close this module</span>
 			</button>

36ecf987e285b5893fd76d95aa014d2d3ba44a77
Leighton Sapir <leigh@incsub.com>
2019-01-04 22:22:34 -0500

    [update] library: input filled function

--- library/js/input-filled.js
+++ library/js/input-filled.js
@@ -1,30 +1,22 @@
 ( function( $ ) {
 
-	// Define global HUI object if it doesn't exist.
-	if ( 'object' !== typeof window.HUI ) {
-		window.HUI = {};
-	}
-
-	HUI.filledClass = function() {
+	HUI.inputFilled = function() {
 
 		$( '.hustle-ui .hustle-input' ).blur( function() {
 
 			const input = $( this );
-			const label = input.parent();
 
-			if ( label.hasClass( 'hustle-field' ) ) {
-
-				if ( '' !== input.val() ) {
-					label.addClass( 'hustle-field-filled' );
-				} else {
-					label.removeClass( 'hustle-field-filled' );
-				}
+			if ( '' !== input.val() ) {
+				input.parent().addClass( 'hustle-status--filled' );
+			} else {
+				input.parent().removeClass( 'hustle-status--filled' );
 			}
+
 		});
 	};
 
 	$( 'body' ).ready( function() {
-		HUI.filledClass();
+		HUI.inputFilled();
 	});
 
 }( jQuery ) );

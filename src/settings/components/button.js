935957db352c8479f449d1abba9abf521066e423
Leighton Sapir <leigh@incsub.com>
2019-01-04 22:26:13 -0500

    [update] showcase: on-load button markup

--- src/settings/components/button.js
+++ src/settings/components/button.js
@@ -4,14 +4,25 @@ export default class Button extends Component {
 	render() {
 		const label = this.props.label ? this.props.label : 'Button';
 		let extraClass = '';
+		let renderLabel = label;
 
 		if ( this.props.extraClass && '' !== this.props.extraClass ) {
 			extraClass = ' ' + this.props.extraClass;
 		}
 
+		if ( this.props.onLoad && "true" === this.props.onLoad ) {
+			renderLabel = (
+				<React.Fragment>
+					<span className="hustle-button-text">{ label }</span>
+					<span className="hustle-loading-text">Form is being submitted. Please, wait a bit...</span>
+					<i className="hustle-icon-loader hustle-loading-icon" aria-hidden="true" />
+				</React.Fragment>
+			);
+		}
+
 		let renderButton = (
 			<button className={ `hustle-button${ extraClass }` }>
-				{ label }
+				{ renderLabel }
 			</button>
 		);
 
@@ -22,7 +33,7 @@ export default class Button extends Component {
 					target={ this.props.target ? this.props.target : '_blank' }
 					className={ `hustle-button${ extraClass }` }
 				>
-					{ label }
+					{ renderLabel }
 				</a>
 			);
 		}

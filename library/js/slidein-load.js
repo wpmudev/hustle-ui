aa39a8f96a67f84221ab38a004d28f15ae813352
Leighton Sapir <leigh@incsub.com>
2019-01-07 19:53:41 -0500

    [update] library: load slide-in function

---
+++ library/js/slidein-load.js
@@ -0,0 +1,104 @@
+( function( $ ) {
+
+	HUI.slideinLoad = function( el, delay ) {
+
+		const slidein = $( el );
+		const content = slidein.find( '.hustle-slidein-content' );
+		const moduleTime = delay - 200;
+		const layoutTime = delay;
+
+		if ( ! slidein.is( '.hustle-slidein' ) ) {
+			return;
+		}
+
+		function reset() {
+			slidein.removeClass( 'hustle-show' );
+		}
+
+		function show() {
+			slidein.addClass( 'hustle-show' );
+		}
+
+		function position() {
+
+			const checkPosition = slidein.data( 'position' );
+
+			let positionX = '';
+			let positionY = '';
+
+			if ( checkPosition.charAt( 0 ).includes( 'n' ) ) {
+				positionX = 'north';
+			}
+
+			if ( checkPosition.charAt( 0 ).includes( 's' ) ) {
+				positionX = 'south';
+			}
+
+			if ( checkPosition.charAt( 0 ).includes( 'e' ) ) {
+				positionX = 'east';
+			}
+
+			if ( checkPosition.charAt( 0 ).includes( 'w' ) ) {
+				positionX = 'west';
+			}
+
+			if ( checkPosition.charAt( 1 ).includes( 'n' ) ) {
+				positionY = 'north';
+			}
+
+			if ( checkPosition.charAt( 1 ).includes( 's' ) ) {
+				positionY = 'south';
+			}
+
+			if ( checkPosition.charAt( 1 ).includes( 'e' ) ) {
+				positionY = 'east';
+			}
+
+			if ( checkPosition.charAt( 1 ).includes( 'w' ) ) {
+				positionY = 'west';
+			}
+
+			if ( 1 === checkPosition.length ) {
+				slidein.addClass( 'hustle-slidein-position--' + positionX );
+			} else {
+				slidein.addClass( 'hustle-slidein-position--' + positionX + '-' + positionY );
+			}
+
+		}
+
+		function animation() {
+			content.addClass( 'hustle-animate-in' );
+		}
+
+		function init() {
+
+			reset();
+			position();
+
+			setTimeout( function() {
+				show();
+			}, moduleTime );
+
+			setTimeout( function() {
+				animation();
+			}, layoutTime );
+
+		}
+
+		init();
+
+		return this;
+	};
+
+	$( 'body' ).ready( function() {
+
+		$( '.hustle-slidein' ).each( function() {
+
+			const slidein = $( this );
+			const delay = $( this ).data( 'delay' );
+
+			HUI.slideinLoad( slidein, delay );
+
+		});
+	});
+}( jQuery ) );

5dd27c8f7112c7e982beae52e45c11a6b2beadfe
Leighton Sapir <leigh@incsub.com>
2019-01-08 19:24:49 -0500

    [update] showcase: import hustle ui js

--- showcase/js/hustle-ui.js
+++ showcase/js/hustle-ui.js
@@ -1,1 +1,57 @@
+import jQuery from 'jquery';
 import '../../library/dist/js/hustle-ui';
+
+( function( $ ) {
+
+	$( 'body' ).ready( function() {
+
+		// Load pop-up
+		$( '.hustle-popup' ).each( function() {
+
+			const popup = $( this );
+			const delay = $( this ).data( 'delay' );
+
+			HUI.popupLoad( popup, delay );
+
+		});
+
+		// Load slide-in
+		$( '.hustle-slidein' ).each( function() {
+
+			const slidein = $( this );
+			const delay = $( this ).data( 'delay' );
+
+			HUI.slideinLoad( slidein, delay );
+
+		});
+
+		// Load inline
+		$( '.hustle-inline' ).each( function() {
+			HUI.inlineLoad( this );
+		});
+
+		// Close module
+		$( '.hustle-button-close' ).each( function() {
+
+			const close = $( this );
+
+			HUI.popupClose( close );
+			HUI.slideinClose( close );
+
+		});
+
+		// Submit button
+		HUI.buttonSubmit( 800 );
+
+		// GDPR checkbox
+		HUI.checkboxGdpr( 800 );
+
+		// Filled input
+		HUI.inputFilled();
+
+		// Required input
+		HUI.inputRequired( 800 );
+
+	});
+
+}( jQuery ) );

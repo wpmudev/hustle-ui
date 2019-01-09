06a9a7891557a6c235cb1f360834ca2a822d3edf
Leighton Sapir <leigh@incsub.com>
2019-01-09 00:22:55 -0500

    [update] library: detect error or success on submit action

--- library/js/button-submit.js
+++ library/js/button-submit.js
@@ -5,41 +5,132 @@
 		window.HUI = {};
 	}
 
-	HUI.buttonSubmit = function( delay ) {
+	HUI.buttonSubmit = function( el, delay ) {
 
-		$( '.hustle-ui .hustle-button-submit' ).on( 'click', function( e ) {
+		const button = $( el );
+		const module = button.closest( '.hustle-ui' );
+		const optin = module.find( '.hustle-optin' );
+		const success = optin.find( '.hustle-success' );
+		const layout = optin.find( '.hustle-layout' );
+		const form = layout.find( '.hustle-layout-form' );
+		const error = form.find( '.hustle-error-message' );
 
-			const button = $( this );
-			const form = button.closest( '.hustle-layout-form' );
-			const gdpr = form.find( '.hustle-gdpr input' );
-			const reqField = form.find( '.hustle-field-required' );
-			const errorField = form.find( '.hustle-field-error' );
-			const errorMessage = form.find( '.hustle-error-message' );
+		if ( ! optin.is( '.hustle-optin' ) ) {
+			return;
+		}
 
-			errorMessage.hide();
+		function resetOnLoad() {
+			success.hide();
+			error.hide();
+		}
+
+		function resetOnClick() {
+
+			const input = form.find( '.hustle-field' );
+			const checkbox = form.find( '.hustle-checkbox' );
+
+			input.removeClass( 'hustle-field-error' );
+			checkbox.removeClass( 'hustle-field-error' );
+			error.hide();
+
+		}
+
+		function animateButton() {
 			button.addClass( 'hustle-button-onload' );
+		}
+
+		function staticButton() {
+			button.removeClass( 'hustle-button-onload' );
+		}
+
+		function checkGdpr() {
+
+			const label = form.find( '.hustle-gdpr' );
+			const input = label.find( 'input' );
+
+			if ( input.is( ':checked' ) ) {
+				label.removeClass( 'hustle-field-error' );
+			} else {
+				label.addClass( 'hustle-field-error' );
+			}
+		}
+
+		function checkRequired() {
+
+			const input = form.find( '.hustle-input' );
+			const label = input.parent();
+
+			label.each( function() {
+
+				const field = $( this );
+
+				if ( field.hasClass( 'hustle-field-required' ) ) {
+
+					if ( '' === field.find( 'input' ).val() ) {
+						field.addClass( 'hustle-field-error' );
+					} else {
+						field.removeClass( 'hustle-field-error' );
+					}
+				}
+			});
+
+		}
+
+		function errorMessage() {
+			error.show();
+		}
+
+		function successMessage() {
+
+			layout.slideUp( delay );
 
 			setTimeout( function() {
-
-				if (
-					gdpr.is( ':checked' ) &&
-					0 === errorField.length &&
-					'' !== reqField.find( 'input' ).val()
-				) {
-					errorMessage.hide();
-				} else {
-					errorMessage.show();
-				}
-
-				button.removeClass( 'hustle-button-onload' );
-
+				success.slideDown();
 			}, delay );
+		}
 
-			e.preventDefault();
+		function init() {
 
-		});
+			resetOnLoad();
+
+			button.on( 'click', function( e ) {
+
+				resetOnClick();
+				animateButton();
+
+				setTimeout( function() {
+
+					checkGdpr();
+					checkRequired();
+
+					if ( form.find( '.hustle-field-error' ).length ) {
+						errorMessage();
+					} else {
+						successMessage();
+					}
+
+					staticButton();
+
+				}, delay );
+
+				e.preventDefault();
+				e.stopPropagation();
+
+			});
+		}
+
+		init();
+
+		return this;
 	};
 
-	HUI.buttonSubmit( 800 );
+	$( '.hustle-button-submit' ).each( function() {
+
+		const button = $( this );
+		const delay = 1000;
+
+		HUI.buttonSubmit( button, delay );
+
+	});
 
 }( jQuery ) );

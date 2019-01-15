bcc6c22781d50c3fec3d47ce8c53d8828b11c482
Leighton Sapir <leigh@incsub.com>
2019-01-14 20:53:13 -0500

    [new] show sample slide-in

---
+++ showcase/js/home-slidein-optin.js
@@ -0,0 +1,118 @@
+( function( $ ) {
+
+	function randomSettings( items ) {
+		return items[ Math.floor( Math.random() * items.length ) ];
+	};
+
+	function disableButton( element ) {
+		element.prop( 'disabled', true );
+	}
+
+	function getSlideinSettings( element, moduleId ) {
+
+		const slidein = element;
+
+		// Module settings
+		const position = [
+			'n',
+			's',
+			'e',
+			'w',
+			'ne',
+			'nw',
+			'se',
+			'sw'
+		];
+		const modulePalette = [
+			'gray-slate',
+			'coffee',
+			'ectoplasm',
+			'blue',
+			'sunrise',
+			'midnight'
+		];
+
+		/**
+		 * Assign new settings to sample module
+		 */
+
+		// Unique ID
+		slidein.attr( 'data-id', moduleId );
+		slidein.addClass( 'hustle-module-' + moduleId );
+
+		// Position
+		slidein.attr( 'data-position', randomSettings( position ) );
+
+		// Module palette
+		slidein.addClass( 'hustle-palette--' + randomSettings( modulePalette ) );
+
+	}
+
+	function renderSlidein( moduleId ) {
+
+		const container = $( '#showcase-slidein-optin' );
+
+		// Templates
+		const renderSlidein = 'templates/slidein-container.html';
+		const renderOptin = [
+			'templates/opt-in/default.html',
+			'templates/opt-in/compact.html',
+			'templates/opt-in/focus-optin.html',
+			'templates/opt-in/focus-content.html'
+		];
+
+		if ( ! $.trim( container.html() ).length ) {
+
+			container.load( renderSlidein, function() {
+
+				const slideinWrapper = $( this );
+				const slideinContainer = slideinWrapper.find( '.hustle-slidein' );
+				const slideinContent = slideinContainer.find( '.hustle-slidein-content' );
+
+				// Get slide-in settings
+				getSlideinSettings( slideinContainer, moduleId );
+
+				// Render opt-in
+				slideinContent.load( randomSettings( renderOptin ) );
+
+				// Open slide-in
+				HUI.slideinLoad( slideinContainer, 0 );
+
+			});
+		}
+	}
+
+	$( window ).on( 'load', function() {
+
+		let moduleId = 0;
+
+		const btnOpen = $( '#showcase-show-optin-slidein' );
+		const btnClose = $( '.hustle-button-close' );
+		const container = $( '#showcase-slidein-optin' );
+
+		btnOpen.on( 'click', function() {
+
+			moduleId = Math.floor( ( Math.random() * 100 ) + 1 );
+
+			renderSlidein( moduleId );
+			disableButton( $( this ) );
+
+		});
+
+		btnClose.each( function() {
+
+			const button = $( this );
+
+			HUI.slideinClose( button );
+
+			button.on( 'click', function() {
+
+				setTimeout( function() {
+					container.empty();
+				}, 1000 );
+			});
+
+		});
+	});
+
+}( jQuery ) );

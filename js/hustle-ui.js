8d95d6e676e86d8ff6e72851840b5325e85067b5
Leighton Sapir <leigh@incsub.com>
2019-01-25 13:43:09 -0500

    Updates

--- js/hustle-ui.js
+++ js/hustle-ui.js
@@ -637,6 +637,114 @@
 
 ( function( $ ) {
 
+	// Enable strict mode
+	'use strict';
+
+	// Define global HUI object if it doesn't exist.
+	if ( 'object' !== typeof window.HUI ) {
+		window.HUI = {};
+	}
+
+	HUI.datepicker = function( el ) {
+
+		const input = $( el );
+
+		let fullMonths = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		];
+
+		let shortMonths = [
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'May',
+			'Jun',
+			'Jul',
+			'Aug',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dec'
+		];
+
+		let fullDays = [
+			'Sunday',
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday'
+		];
+
+		let minDays = [
+			'Su',
+			'Mo',
+			'Tu',
+			'We',
+			'Th',
+			'Fr',
+			'Sa'
+		];
+
+		let shortDays = [
+			'Sun',
+			'Mon',
+			'Tue',
+			'Wed',
+			'Thu',
+			'Fri',
+			'Sat'
+		];
+
+		$( '.hustle-ui' ).each( function() {
+
+			const container = $( this );
+			const moduleId = container.data( 'id' );
+			const element  = container.find( input );
+
+			element.datepicker({
+				monthNames: fullMonths,
+				monthNamesShort: shortMonths,
+				dayNames: fullDays,
+				dayNamesMin: minDays,
+				dayNamesShort: shortDays,
+				minDate: ( '' !== element.data( 'min-date' ) ) ? element.data( 'min-date' ) : null,
+				changeMonth: true,
+				changeYear: true,
+				isRTL: ( true === element.data( 'rtl-support' ) ) ? true : false,
+				showButtonPanel: true,
+				beforeShow: function( input, inst ) {
+
+					( inst.dpDiv ).addClass( 'hustle-calendar' );
+					( inst.dpDiv ).addClass( 'hustle-module-' + container.data( 'id' ) );
+
+					if ( '' !== container.data( 'calendar-palette' ) ) {
+						( inst.dpDiv ).addClass( 'hustle-palette--' + container.data( 'calendar-palette' ) );
+					}
+				}
+			});
+		});
+	};
+
+	HUI.datepicker( '.hustle-date' );
+
+}( jQuery ) );
+
+( function( $ ) {
+
 	'use strict';
 
 	// Define global HUI object if it doesn't exist.
@@ -7812,3 +7920,4 @@
 	});
 
 }( jQuery ) );
+

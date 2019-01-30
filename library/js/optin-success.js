417655cec19a361c411315933228554ac563b3de
Leighton Sapir <leigh@incsub.com>
2019-01-29 22:08:53 -0500

    [New] Opt-in success function

---
+++ library/js/optin-success.js
@@ -0,0 +1,39 @@
+( function( $ ) {
+
+	'use strict';
+
+	// Define global HUI object if it doesn't exist.
+	if ( 'object' !== typeof window.HUI ) {
+		window.HUI = {};
+	}
+
+	HUI.optinSuccess = function( el ) {
+
+		const success = $( el );
+		const container = success.closest( '.hustle-ui' );
+		const layout = container.find( '.hustle-layout' );
+
+		if ( ! success.is( '.hustle-success' ) && '' === success.length ) {
+			return;
+		}
+
+		function successMessage() {
+
+			layout.slideUp( 800 );
+
+			setTimeout( function() {
+				success.slideDown();
+			}, 800 );
+		}
+
+		function init() {
+			successMessage();
+		}
+
+		init();
+
+		return this;
+
+	};
+
+}( jQuery ) );

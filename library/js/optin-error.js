684db2ff000a8df59022b92dd7d0a97b5a9d1000
Leighton Sapir <leigh@incsub.com>
2019-01-29 22:09:10 -0500

    [New] Opt-in error function

---
+++ library/js/optin-error.js
@@ -0,0 +1,28 @@
+( function( $ ) {
+
+	'use strict';
+
+	// Define global HUI object if it doesn't exist.
+	if ( 'object' !== typeof window.HUI ) {
+		window.HUI = {};
+	}
+
+	HUI.optinError = function( el ) {
+
+		const message = $( el );
+
+		if ( ! message.is( '.hustle-error-message' ) && '' === message.length ) {
+			return;
+		}
+
+		function init() {
+			message.show();
+		}
+
+		init();
+
+		return this;
+
+	};
+
+}( jQuery ) );

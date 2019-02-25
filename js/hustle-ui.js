467c852d4fc8d3bb248bc438aeaf2c6deafa175a
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2019-02-25 12:01:17 -0500

    Updates

--- js/hustle-ui.js
+++ js/hustle-ui.js
@@ -692,6 +692,51 @@
 
 }( jQuery ) );
 
+( function( $, window ) {
+
+	// Enable strict mode
+	'use strict';
+
+	// Define global HUI object if it doesn't exist.
+	if ( 'object' !== typeof window.HUI ) {
+		window.HUI = {};
+	}
+
+	HUI.inlineResize = function( el ) {
+
+		const element = $( el );
+		const elWidth = element.width();
+
+		if ( ! element.is( '.hustle-inline' ) ) {
+			return;
+		}
+
+		function init() {
+
+			if ( 783 < Math.max( document.documentElement.clientWidth, window.innerWidth || 0 ) ) {
+
+				if ( element.hasClass( 'hustle-size--small' ) ) {
+
+					if ( 783 < elWidth ) {
+						element.removeClass( 'hustle-size--small' );
+					}
+				} else {
+
+					if ( 782 > elWidth ) {
+						element.addClass( 'hustle-size--small' );
+					}
+				}
+			}
+
+		}
+
+		init();
+
+		return this;
+	};
+
+}( jQuery ) );
+
 ( function( $ ) {
 
 	'use strict';

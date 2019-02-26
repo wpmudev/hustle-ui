3f6139108deb15f81a1f5873214a0dc10303764b
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2019-02-25 19:41:33 -0500

    [new] [library] inline small class function

---
+++ library/js/inline-resize.js
@@ -0,0 +1,44 @@
+( function( $ ) {
+
+	// Enable strict mode
+	'use strict';
+
+	// Define global HUI object if it doesn't exist.
+	if ( 'object' !== typeof window.HUI ) {
+		window.HUI = {};
+	}
+
+	HUI.inlineResize = function( el ) {
+
+		const element = $( el );
+		const elWidth = element.width();
+
+		if ( ! element.is( '.hustle-inline' ) ) {
+			return;
+		}
+
+		function init() {
+
+			if ( 783 < Math.max( document.documentElement.clientWidth, window.innerWidth || 0 ) ) {
+
+				if ( element.hasClass( 'hustle-size--small' ) ) {
+
+					if ( 783 < elWidth ) {
+						element.removeClass( 'hustle-size--small' );
+					}
+				} else {
+
+					if ( 782 > elWidth ) {
+						element.addClass( 'hustle-size--small' );
+					}
+				}
+			}
+
+		}
+
+		init();
+
+		return this;
+	};
+
+}( jQuery ) );

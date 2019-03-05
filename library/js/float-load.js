b9e455eb1e619449d3ada0a9ca079e250acbe846
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2019-03-05 10:56:57 -0500

    [new] [library] loading float js function

---
+++ library/js/float-load.js
@@ -0,0 +1,51 @@
+( function( $ ) {
+
+	'use strict';
+
+	// Define global HUI object if it doesn't exist.
+	if ( 'object' !== typeof window.HUI ) {
+		window.HUI = {};
+	}
+
+	HUI.floatLoad = function( el ) {
+
+		const float = $( el );
+		const content = float.find( '.hustle-float-content' );
+
+		if ( ! float.is( '.hustle-float' ) ) {
+			return;
+		}
+
+		function reset() {
+			float.removeClass( 'hustle-show' );
+		}
+
+		function show() {
+			float.addClass( 'hustle-show' );
+		}
+
+		function animation() {
+			content.addClass( 'hustle-animate-in' );
+		}
+
+		function init() {
+
+			reset();
+
+			// Module time
+			setTimeout( function() {
+				show();
+			}, 600 );
+
+			// Layout time
+			setTimeout( function() {
+				animation();
+			}, 800 );
+
+		}
+
+		init();
+
+		return this;
+	};
+}( jQuery ) );

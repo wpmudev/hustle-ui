65cc5e5e589b0c1b25403622a645d1882e6fec62
Pawan Kumar <pwnkumar201@gmail.com>
2022-12-09 14:40:34 +0530

    ♿ aria(popup): added hustle popup keyboard accessiblity.

--- library/js/popup-load.js
+++ library/js/popup-load.js
@@ -78,6 +78,34 @@
 
 		}
 
+		function trapFocus( elem ) {
+			var element = document.getElementById( elem );
+			var focusableEls = element.querySelectorAll( 'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])' );
+			var firstFocusableEl = focusableEls[0];
+			var lastFocusableEl = focusableEls[focusableEls.length - 1];
+			var KEYCODE_TAB = 9;
+
+			element.addEventListener( 'keydown', function( e ) {
+				var isTabPressed = ( 'Tab' === e.key || e.keyCode === KEYCODE_TAB );
+
+			if ( ! isTabPressed ) {
+				return;
+			}
+
+			if ( e.shiftKey ) /* shift + tab */ {
+				if ( document.activeElement === firstFocusableEl ) {
+				lastFocusableEl.focus();
+					e.preventDefault();
+				}
+				} else /* tab */ {
+				if ( document.activeElement === lastFocusableEl ) {
+				firstFocusableEl.focus();
+					e.preventDefault();
+				}
+				}
+			});
+		}
+
 		function init() {
 
 			popup.removeClass( 'hustle-show' );
@@ -91,6 +119,8 @@
 				animationIn();
 				$( document ).trigger( 'hustle:module:displayed', popup );
 			}, 200 );
+
+			trapFocus( popup[0].id );
 
 			// resize iframes, object and videos
 			resizeObjectsInContent();
